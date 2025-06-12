2e0b87686e075d5bfe16cb8cd975cd69a0acfff2
root <root@raspberrypi>
2025-06-12 18:22:52 +0200

    New Release - We can now add On/Off switches which are active_low or active_high

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ To install:
 * Run ```sudo pip3 install RPi.GPIO```
 * Go in your Domoticz directory, open the plugins directory.
 * Navigate to the directory using a command line
-* Run: ```git clone https://github.com/dnpwwo/Domoticz-GPIO-Plugin.git```
+* Run: ```git clone https://github.com/domcars0/Domoticz-GPIO-Plugin.git```
 * Restart Domoticz.
 
 In the web UI, navigate to the Hardware page.  In the hardware dropdown there will be an entry called "Raspberry Pi GPIO".
@@ -30,14 +30,17 @@ To update:
 
 | Field | Information|
 | ----- | ---------- |
-| Output Pins | Comma delimited list of output (relay) pins. Format is pin number colon NO or NC (e.g 39:NO) |
+| Output Pins | Comma delimited list of output (relay) pins. Format is pin number colon NO or NC or TO colon 0 or 1 (e.g 39:TO:1) |
 | Input Pins | Comma delimited list of board pin numbers (1-40) to check for input |
 | Switch Debounce | Time (ms) between valid switch presses, prevents multi reports of events |
 | Heartbeat Frequency | Determines how often Input Pins are checked for values |
 | Debug | When true the logging level will be much higher to aid with troubleshooting |
+
+Notes : If you want to change the Type of an output button created by the plugin use the Device setup button (change it in the plugin will do nothing)
 
 ## Change log
 
 | Version | Information|
 | ----- | ---------- |
 | 1.0.0 | Initial upload version |
+| 2.0.0 | Can now create Output types : On/Off Button (TO), Push Off Button (NC) or Push On Button (NO), active_low (1) or active_high (0) |
--- plugin.py
+++ plugin.py
@@ -1,15 +1,15 @@
 # Domoticz-GPIO-Plugin
 #
-# Author: Dnpwwo
+# Authors: Dnpwwo, Domcars0
 #
 #
 """
-<plugin key="RPiGPIO" name="Raspberry Pi GPIO" author="dnpwwo" version="1.0.0" wikilink="https://github.com/dnpwwo/Domoticz-GPIO-Plugin" >
+<plugin key="RPiGPIO" name="Raspberry Pi GPIO" author="dnpwwo,domcars0" version="2.0.0" wikilink="https://github.com/dnpwwo/Domoticz-GPIO-Plugin" >
     <description>
         <h2>Domoticz GPIO Plugin</h2><br/>
         <h3>Configuration</h3>
         <ul style="list-style-type:square">
-            <li style="line-height:normal">Output Pins - Comma delimited list of output (relay) pins. Format is pin number colon NO or NC (e.g 39:NO)</li>
+            <li style="line-height:normal">Output Pins - Comma delimited list of output (relay) pins. Format is pin number colon NC|NO|TO colon 0|1  ( e.g 39:TO:1, where TO is for "On/Off" Button, NO is "Push On" Button, NC is a "Push Off" Button. 1 means active_low switch, 0 means active_high switch ) </li>
             <li style="line-height:normal">Input Pins - Comma delimited list of board pin numbers (1-40) to check for input</li>
             <li style="line-height:normal">Switch Debounce - Time (ms) between valid switch presses, prevents multi reports of events</li>
             <li style="line-height:normal">Heartbeat Frequency - Determines how often Input Pins are checked for values</li>
@@ -60,83 +60,136 @@
 import Domoticz
 import RPi.GPIO as GPIO
 
-def onStart():
-    if Parameters["Mode6"] != "0":
-        Domoticz.Log("Parameter is: '"+Parameters["Mode6"]+"'")
-        Domoticz.Debugging(int(Parameters["Mode6"]))
-        DumpConfigToLog()
+class BasePlugin:
+	enabled = False
+	def __init__(self):
+		return
 
-    GPIO.setmode(GPIO.BOARD)
-    Domoticz.Heartbeat(int(Parameters["Mode5"]))
-    Domoticz.Log("RPi.GPIO imported, Version: "+str(GPIO.VERSION)+", Raspberry Pi board revision: "+str(GPIO.RPI_INFO['P1_REVISION']))
+	def onStart(self):
+		global ActiveLow 
+		ActiveLow = {} 
+
+		if Parameters["Mode6"] != "0":
+			Domoticz.Log("Parameter is: '"+Parameters["Mode6"]+"'")
+			Domoticz.Debugging(int(Parameters["Mode6"]))
+			DumpConfigToLog()
+
+		GPIO.setmode(GPIO.BOARD)
+		Domoticz.Heartbeat(int(Parameters["Mode5"]))
+		Domoticz.Log("RPi.GPIO imported, Version: "+str(GPIO.VERSION)+", Raspberry Pi board revision: "+str(GPIO.RPI_INFO['P1_REVISION']))
     
-    # Process Output Pins
-    if (len(Parameters["Mode1"]) > 0):
-        try:
-            outputPins = Parameters["Mode1"].split(',')
-            for pin in outputPins:
-                items = pin.split(':')
-                pinNo = int(items[0])
-                if not (pinNo in Devices):
-                    Domoticz.Log("Creating Output device #"+items[0])
-                    Domoticz.Device(Name="Output "+items[0], Unit=pinNo, TypeName="Push Off" if items[1] == "NC" else "Push On").Create()
-                GPIO.setup(pinNo, GPIO.OUT, initial=GPIO.HIGH if items[1] == "NC" else GPIO.LOW)
-                UpdateDevice(pinNo, GPIO.input(pinNo), "", 0)
-        except Exception as inst:
-            Domoticz.Error("Exception in onStart, processing Output Pins")
-            Domoticz.Error("Exception detail: '"+str(inst)+"'")
-            raise
+		# Process Output Pins
+		if (len(Parameters["Mode1"]) > 0):
+			try:
+				outputPins = Parameters["Mode1"].split(',')
+				for pin in outputPins:
+					items = pin.split(':')
+					pinNo = int(items[0])
+					if not (pinNo in Devices):
+						Domoticz.Log("Creating Output device #"+items[0])
+						Domoticz.Device(Name="Output "+items[0], Unit=pinNo, TypeName="Push Off" if items[1] == "NC" else "Push On" if items[1] == "NO" else "On/Off" ).Create()
 
-    # Process Input Pins
-    if (len(Parameters["Mode2"]) > 0):
-        try:
-            inputPins = Parameters["Mode2"].split(',')
-            for pin in inputPins:
-                pinNo = int(pin)
-                if not (pinNo in Devices):
-                    Domoticz.Log("Creating Input device #"+pin)
-                    Domoticz.Device(Name="Input "+pin, Unit=pinNo, TypeName="Contact").Create()
-                GPIO.setup(pinNo, GPIO.IN)
-                GPIO.add_event_detect(pinNo, GPIO.BOTH, callback=gpioCallback, bouncetime=int(Parameters["Mode3"]))
-                UpdateDevice(pinNo, GPIO.input(pinNo), "", 0)
-        except Exception as inst:
-            Domoticz.Error("Exception in onStart, processing Input Pins")
-            Domoticz.Error("Exception detail: '"+str(inst)+"'")
-            raise
+					GPIO.setup(pinNo, GPIO.OUT, initial=GPIO.HIGH if items[1] == "NC" or int(items[2]) == 1 else GPIO.LOW)
+					if items[2] == None:
+						ActiveLow[pinNo] = 0
+					else:
+						ActiveLow[pinNo] = int(items[2])
 
-def gpioCallback(channel):
-    channelValue = GPIO.input(channel)
-    Domoticz.Debug("gpioCallback called, Channel: "+str(channel)+", Value: "+str(channelValue))
-    UpdateDevice(channel, channelValue, "On" if channelValue == GPIO.HIGH else "Off", 0)
+					UpdateDevice(pinNo, GPIO.input(pinNo), "", 0)
+					Domoticz.Log(" Active high/low device :"+items[0]+", value = "+str(ActiveLow[pinNo]))
+
+			except Exception as inst:
+				Domoticz.Error("Exception in onStart, processing Output Pins")
+				Domoticz.Error("Exception detail: '"+str(inst)+"'")
+				raise
+
+			# Process Input Pins
+			if (len(Parameters["Mode2"]) > 0):
+				try:
+					inputPins = Parameters["Mode2"].split(',')
+					for pin in inputPins:
+						pinNo = int(pin)
+						if not (pinNo in Devices):
+							Domoticz.Log("Creating Input device #"+pin)
+							Domoticz.Device(Name="Input "+pin, Unit=pinNo, TypeName="Contact").Create()
+						GPIO.setup(pinNo, GPIO.IN)
+						GPIO.add_event_detect(pinNo, GPIO.BOTH, callback=gpioCallback, bouncetime=int(Parameters["Mode3"]))
+						UpdateDevice(pinNo, GPIO.input(pinNo), "", 0)
+				except Exception as inst:
+					Domoticz.Error("Exception in onStart, processing Input Pins")
+					Domoticz.Error("Exception detail: '"+str(inst)+"'")
+					raise
+
+	def gpioCallback(self,channel):
+		channelValue = GPIO.input(channel)
+		Domoticz.Debug("gpioCallback called, Channel: "+str(channel)+", Value: "+str(channelValue))
+		UpdateDevice(channel, channelValue, "On" if channelValue == GPIO.HIGH else "Off", 0)
+
+	def onCommand(self,Unit, Command, Level, Hue):
+		global ActiveLow
+		Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+		if ActiveLow[Unit] == 1:
+			GPIO.output(Unit, GPIO.HIGH if Command == "Off" else GPIO.LOW)
+		else:
+			GPIO.output(Unit, GPIO.LOW if Command == "Off" else GPIO.HIGH)
+        
+		UpdateDevice(Unit, GPIO.input(Unit), Command, 0)
+
+	def onStop(self):
+		Domoticz.Debug("onStop called")
+		GPIO.cleanup()
+
+	def onHeartbeat(self):
+		Domoticz.Debug("onHeartbeat called")
+
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+	global _plugin
+	_plugin.onStart()
+
+def onConnect(Connection, Status, Description):
+	global _plugin
+	_plugin.onConnect(Connection, Status, Description)
 
 def onCommand(Unit, Command, Level, Hue):
-    Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
-    GPIO.output(Unit, GPIO.HIGH if Command == "On" else GPIO.LOW)
-    UpdateDevice(Unit, GPIO.input(Unit), Command, 0)
+	global _plugin
+	_plugin.onCommand(Unit, Command, Level, Hue)
 
-def onStop():
-    Domoticz.Debug("onStop called")
-    GPIO.cleanup()
+def onMessage(Connection, Data):
+	global _plugin
+	_plugin.onMessage(Connection, Data)
+
+def onHeartbeat():
+	global _plugin
+	_plugin.onHeartbeat()
 
 # Generic helper functions
 def DumpConfigToLog():
-    for x in Parameters:
-        if Parameters[x] != "":
-            Domoticz.Log( "'" + x + "':'" + str(Parameters[x]) + "'")
-    Domoticz.Log("Device count: " + str(len(Devices)))
-    for x in Devices:
-        Domoticz.Log("Device:           " + str(x) + " - " + str(Devices[x]))
-        Domoticz.Log("Device ID:       '" + str(Devices[x].ID) + "'")
-        Domoticz.Log("Device Name:     '" + Devices[x].Name + "'")
-        Domoticz.Log("Device nValue:    " + str(Devices[x].nValue))
-        Domoticz.Log("Device sValue:   '" + Devices[x].sValue + "'")
-        Domoticz.Log("Device LastLevel: " + str(Devices[x].LastLevel))
-    return
+	for x in Parameters:
+		if Parameters[x] != "":
+			Domoticz.Log( "'" + x + "':'" + str(Parameters[x]) + "'")
+
+	Domoticz.Log("Device count: " + str(len(Devices)))
+
+	for x in Devices:
+		Domoticz.Log("Device:           " + str(x) + " - " + str(Devices[x]))
+		Domoticz.Log("Device ID:       '" + str(Devices[x].ID) + "'")
+		Domoticz.Log("Device Name:     '" + Devices[x].Name + "'")
+		Domoticz.Log("Device nValue:    " + str(Devices[x].nValue))
+		Domoticz.Log("Device sValue:   '" + Devices[x].sValue + "'")
+		Domoticz.Log("Device LastLevel: " + str(Devices[x].LastLevel))
+	return
     
 def UpdateDevice(Unit, nValue, sValue, TimedOut):
-    # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
-    if (Unit in Devices):
-        if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue) or (Devices[Unit].TimedOut != TimedOut):
-            Devices[Unit].Update(nValue=nValue, sValue=str(sValue), TimedOut=TimedOut)
-            Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
-    return
+	global ActiveLow
+	if ActiveLow[Unit] :
+		nValue = 1 if nValue == 0 else 0
+		sValue = "Off" if sValue == "On" else "On"
+	# Make sure that the Domoticz device still exists (they can be deleted) before updating it 
+	if (Unit in Devices):
+		if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue) or (Devices[Unit].TimedOut != TimedOut):
+			Devices[Unit].Update(nValue=nValue, sValue=str(sValue), TimedOut=TimedOut)
+			Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' : active_low :"+str(ActiveLow[Unit])+" ("+Devices[Unit].Name+")")
+	return
